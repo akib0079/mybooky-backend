8336b3a4bc2be7792cb5cb5fbf13dc99227b46bf
Akib Zawayed <56231072+akib0079@users.noreply.github.com>
2022-05-14 01:04:40 +0600

    All book data fecting complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const port = process.env.PORT || 5000
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 
@@ -9,9 +10,34 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Connecting application.
+const uri = `mongodb+srv://${process.env.DataBase_UName}:${process.env.DataBase_Pass}@cluster0.1n7br.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function runDataBase() {
+    try {
+
+        app.get('/books', async (req, res) => {
+            await client.connect();
+            const dName = client.db("MyBookyD_Base").collection("Books");
+
+            const query = {};
+            const cursor = dName.find(query);
+
+            const allBooks = await cursor.toArray();
+            res.send(allBooks);
+        })
+    }
+    finally {
+
+    }
+}
+
+runDataBase().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-    res.send('Connecting...! Connected now')
+    res.send('Connecting...! Connected now..')
 })
 
 app.listen(port, () => {
